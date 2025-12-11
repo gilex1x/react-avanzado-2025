55f0ce4a24c9659a9f56ea93f0b2dfd604ae2ca2
root <root@DESKTOP-JGMP2PO.localdomain>
2025-12-11 17:02:19 -0500

    [ADD]: Render props examples

--- src/components/RenderProps2/ChildComponent.tsx
+++ src/components/RenderProps2/ChildComponent.tsx
@@ -1,6 +1,17 @@
-const ChildComponent = () => {
+import React from "react";
+
+interface ChildComponentProps {
+  data: string[];
+}
+
+const ChildComponent: React.FC<ChildComponentProps> = ({ data }) => {
   return (
     <>
+      {data.map((item, index) =>
+        <li key={index}>
+          {item}
+        </li>
+      )}
     </>
   );
 };
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,16 @@
-const ParentComponent = () => {
+import { useState } from "react";
+
+interface ParentComponentProps {
+    render: (data: string[]) => React.ReactNode
+}
+
+const ParentComponent: React.FC<ParentComponentProps> = ({ render }) => {
+    const [data] = useState<string[]>(["Manzan", "Banano", "Pera"]);
+    return (
+        <ul>
+            {render(data)}
+        </ul>
+    )
 };
 
 export default ParentComponent;
