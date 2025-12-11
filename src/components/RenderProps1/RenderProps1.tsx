eb70f6d6865784ac728cbf9c2369fa757b39a953
root <root@DESKTOP-JGMP2PO.localdomain>
2025-12-11 16:39:40 -0500

    [REMOVE] node_modues

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,22 @@
+import React from "react";
+
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element;
+}
+
+export const ChildComponent: React.FC<ChildComponentProps> = ({ render }) => {
+  const name = 'Hola from render'
+  return (
+    <div>
+      {render(name)}
+    </div>
+  );
+};
+
 export const ParentComponent = () => {
   return (
     <>
+      <ChildComponent render={(name) => <p>Texto: {name}!</p>} />
     </>
   );
 };
