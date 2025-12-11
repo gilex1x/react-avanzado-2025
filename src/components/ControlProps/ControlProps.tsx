518b21373b51654636ad837f2f442356d4049e43
root <root@DESKTOP-JGMP2PO.localdomain>
2025-12-11 17:24:16 -0500

    [ADD]: Controlled component class

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
+
+interface ChildToggleComponentProps {
+  isToggled?: boolean;
+  onToggled?: (value: boolean) => void;
+}
+
+export const ChildToggleComponent: React.FC<ChildToggleComponentProps> = ({ isToggled = false, onToggled }) => {
+
+  const [toggleState, setToggledState] = useState<boolean>(isToggled);
+  const handleToggle = () => {
+    const newToggle = !toggleState;
+    setToggledState(newToggle);
+    if (onToggled) {
+      onToggled(newToggle);
+    }
+  }
+  return (
+    <>
+      <button onClick={handleToggle}>
+        Es toggle: {toggleState ? "ON" : "OFF"}
+      </button>
+    </>
+  );
+};
+
 export const ParentComponent = () => {
+  const [childToggle, setChildToggle] = useState<boolean>(false);
   return (
     <div>
+      <span>El child es toggle: {childToggle ? "ON" : "OFF"}</span>
+      <ChildToggleComponent isToggled={childToggle} onToggled={setChildToggle} />
     </div>
   );
 };
